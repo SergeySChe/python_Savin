177b4836b620c4a720e752a96bafb2462478287f
SergeySChe <83774059+SergeySChe@users.noreply.github.com>
2021-07-01 21:58:49 +0300

    lesson_7

---
+++ Savin_Sergey_dz_7/task_7_1.py
@@ -0,0 +1,16 @@
+print('Hello World')
+
+
+import os
+
+pattern = {'my_project': ['settings', 'mainapp', 'adminapp', 'authapp']}
+for root, folders in pattern.items():
+    if os.path.exists(root):
+        print(root, 'существует')
+    else:
+        for folder in folders:
+            cur_dir = os.path.join(root, folder)
+            if os.path.exists(cur_dir):
+                print(cur_dir, 'существует')
+            else:
+                os.makedirs(cur_dir)
